da181ccab3b763fa13871fb1e17454f0325201fd
SantiJavaOk <santore@gmail.com>
2020-12-21 12:25:51 -0300

    Cambios en la clase log

--- src/main/java/LimpiaTuti/Log.java
+++ src/main/java/LimpiaTuti/Log.java
@@ -2,5 +2,17 @@ package LimpiaTuti;
 
 public class Log {
 	
+	//Esto es un cambio en log
+	
+	private String logging;
 
+	public String getLogging() {
+		return logging;
+	}
+
+	public void setLogging(String logging) {
+		this.logging = logging;
+	}
+	
+	
 }
